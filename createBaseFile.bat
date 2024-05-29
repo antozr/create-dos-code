cb27f0748f41a8973787ae580b22b89d25357d1d
anto__dev <antonidwm@gmail.com>
2024-05-29 09:37:46 +0200

    ADD icone

--- createBaseFile.bat
+++ createBaseFile.bat
@@ -23,37 +23,57 @@ cd %namePath%%nameDos%
 echo Choose beetwen  1:REACT / 2:VUE / 3:NEXT / 4:VANILLA 
 set /p nameExtension="What thech you choose ? "
 echo You choose : %nameExtension%
-set /p otherExtention="Do you want install other lib for ANIMATION and / or DATABASE ?   1:ANIMATION / 2:DB / 3:BOTH / 4:NO"
+set /p otherExtention="Do you want install other lib for ANIMATION ?   1:ANIMATION 2:NO"
 
-if %otherExtention%==1 (
-    if %nameExtension%==1 (
-        npm i --save-dev @gsap/react framer-motion react-spring
-    )
-    else if %nameExtension%==2 (
-       npm i gsap  vuex@next animejs 
-    )
-    if %nameExtension%==3 (
-        npm i --save-dev @gsap/react framer-motion react-spring
-    )
-    else (
-        npm i gsap animejs 
-    )
-)
+@REM if %otherExtention%==1 (
+@REM     if %nameExtension%==1 (
+@REM         npm i --save-dev @gsap/react framer-motion react-spring
+@REM     )
+@REM     else if %nameExtension%==2 (
+@REM        npm i gsap  vuex@next animejs lenis
+@REM     )
+@REM     if %nameExtension%==3 (
+@REM         npm i --save-dev @gsap/react framer-motion react-spring
+@REM     )
+@REM     else (
+@REM         npm i gsap animejs lenis
+@REM     )
+@REM )
 
 if %nameExtension%==1 (
     npm create vite@latest my-vue-app --  react sass
     start %namePath%%nameDos%
+    set -p dbExtension="Do you want a DATABASE lib (with node.js, express & mangoose/MangoDB) ?       1: YES / 2: NO"
+   if %otherExtention%==1 AND %DATABASE%==1 (
+     npm i --save-dev @gsap/react framer-motion react-spring nodejs express mangodb
+   )else (
+    npm i --save-dev @gsap/react framer-motion react-spring
+   )
+
     )
 else if %nameExtension%==2 (
     npm create vite@latest my-vue-app --  vue sass
+    if %otherExtention%==1 (
+     npm i gsap  vuex@next animejs lenis
+    )
+
     start %namePath%%nameDos%
     )
 else if %nameExtension%==3 (
     npx create-next-app@latest sass
+    set -p dbExtension="Do you want a DATABASE lib (with node.js, express & mangoose/MangoDB) ?       1: YES / 2: NO"
+   if %otherExtention%==1 AND %DATABASE%==1 (
+     npm i --save-dev @gsap/react framer-motion react-spring nodejs express mangodb
+   )else (
+    npm i --save-dev @gsap/react framer-motion react-spring
+   )
     start %namePath%%nameDos%
     )
 else if %nameExtension%==4 (
     npm install --save-dev parcel sass
+    if %otherExtention%==1 (
+    npm i gsap animejs lenis
+    )
     start %namePath%%nameDos%
     )
 else (
